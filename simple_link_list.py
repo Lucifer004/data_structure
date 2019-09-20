3afd5f33828d83d5a746df87e85e010a2e138392
aayush-scalex <aayush@scaletechnologies.com>
2019-09-20 14:04:16 +0530

    updated the singly-linklist

--- simple_link_list.py
+++ simple_link_list.py
@@ -10,23 +10,36 @@ class link_list:
     def __init__(self):
         self.start = None
 
-    def delete_node(self):
+    def delete_node(self,value):
         if self.start == None:
             print('The link-list is empty!')
         else:
             temp = self.start
-            while temp.next!= None:
-                temp = temp.next
-            temp.next=None
+            if temp and temp.data == value:#if the deletion element is first node, move the head to next_node and set the previous node to None.
+                self.start = temp.next
+                temp = None
+            else:#if the element is not first node.
+                prev = None
+                while temp and temp.data!=value:
+                    prev = temp
+                    temp = temp.next
+                
+                if temp is None:
+                    print('\nelement %s not found in the list'%(value))
+                    return None
+                prev.next = temp.next
+                temp = None
 
+                
     def view(self):
         if self.start==None:
             print('list is empty')
         else:
             temp = self.start
-            while temp.next!=None:
+            while temp.next is not None:
                 print(temp.data,end=' ')
                 temp=temp.next
+            print(temp.data,end=' ')
     def insert_node(self,value):
         new_node = node(value)
         if self.start==None: #if there is no node present at start, hence start is the first node.
@@ -40,4 +53,11 @@ class link_list:
 if __name__ == "__main__":
     mylist = link_list()
     mylist.insert_node(1)
+    mylist.insert_node(2)
+    mylist.insert_node(4)
+    mylist.insert_node(5)
+    print('link-list after insertion--' )
+    mylist.view()
+    mylist.delete_node(6)# enter the node to be deleted-->
+    print('\nlink-list after deleting the node--')
     mylist.view()
